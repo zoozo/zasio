a548b483b5a082c92aa58d89ae68b78991875170
Charles Chuang <chinan33t@gmail.com>
2016-11-29 05:10:57 +0000

    :

---
+++ src/exception.hpp
@@ -0,0 +1,33 @@
+#ifndef ZOOZO_ZASIO_EXCEPTION_H
+#define ZOOZO_ZASIO_EXCEPTION_H
+
+#include <exception>
+#include <boost/system/error_code.hpp>
+
+namespace zoozo{
+namespace zasio{
+    class zexception : public std::exception{
+        public:
+            explicit zexception(boost::system::error_code ec, const std::string& what )
+                :_code(ec), _what(what) { }
+            explicit zexception(boost::system::error_code ec)
+                :_code(ec), _what(ec.message()) {}
+
+            ~zexception() throw() {}
+
+            boost::system::error_code code() const throw()
+            {
+                return _code;
+            }
+            virtual const char* what() const throw()
+            {
+                return _what.c_str();
+            }
+
+        private:
+            boost::system::error_code _code;
+            const std::string _what;
+    };
+}
+}
+#endif
